cd76710b9f31e7b9a353774a453feb3f7c6e575f
winju <winju@wemade.com>
2024-05-06 23:01:36 +0900

    [feat] Lending page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,17 @@
 import * as React from 'react';
 import type { HeadFC, PageProps } from 'gatsby';
 import LedingApply from '../components/Lending/LendingApply/LedingApply';
+import LendingReservation from '../components/Lending/LendingReservation/LendingReservation';
+import LendingStore from '../components/Lending/LendingStore/LendingStore';
+import LendingReview from '../components/Lending/LendingReview/LendingReview';
 
 const IndexPage: React.FC<PageProps> = () => {
   return (
     <main>
       <LedingApply />
+      <LendingReservation />
+      <LendingStore />
+      <LendingReview />
     </main>
   );
 };
