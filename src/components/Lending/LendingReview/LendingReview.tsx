c37ed9e848c1382945470c479dd2988333905d4d
winju <winju@wemade.com>
2024-05-06 23:00:47 +0900

    [styles] 서비스소개 페이지 하단 두개 컴포넌트 제작

---
+++ src/components/Lending/LendingReview/LendingReview.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import * as styles from './LendingReview.module.scss';
+import LendingFlexContent from '../BaseComponents/LendingFlexContent';
+import { StaticImage } from 'gatsby-plugin-image';
+
+const LendingReview = () => {
+  return (
+    <section className={styles.LendingStoreWrapper}>
+      <LendingFlexContent>
+        <div>
+          <LendingFlexContent.Header>
+            <div>미용, 목욕 꿀팁을</div>
+            <div>공유하고</div>
+            <div>이야기해봐요!</div>
+          </LendingFlexContent.Header>
+          <LendingFlexContent.Description>
+            <div>솔직한 후기를</div>
+            <div>사진과 리뷰로 확인해보세요</div>
+          </LendingFlexContent.Description>
+        </div>
+        <LendingFlexContent.Image>
+          <StaticImage src="../../../images/lending/lending_reservation_1.png" alt="LendingReservation1" />
+        </LendingFlexContent.Image>
+      </LendingFlexContent>
+    </section>
+  );
+};
+
+export default LendingReview;
