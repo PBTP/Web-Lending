2546f4de4f42622380afdf8201373f680f942cdb
winju <winju@wemade.com>
2024-05-06 22:59:54 +0900

    [feat] LendingFlexContent 컴파운드 컴포넌트 패턴 적용

---
+++ src/components/Lending/BaseComponents/LendingFlexContent.tsx
@@ -0,0 +1,32 @@
+import React, { ReactNode } from 'react';
+import * as styles from './LendingFlexContent.module.scss';
+
+interface ILendingFlexContentBaseProps {
+  children: ReactNode;
+}
+
+interface HeaderProps extends ILendingFlexContentBaseProps {}
+interface DescriptionProps extends ILendingFlexContentBaseProps {}
+interface ImageProps extends ILendingFlexContentBaseProps {}
+
+const LendingFlexContent = ({ children }: ILendingFlexContentBaseProps) => {
+  return <div className={styles.LendingFlexContent}>{children}</div>;
+};
+
+const Header = ({ children }: HeaderProps) => {
+  return <div className={styles.Header}>{children}</div>;
+};
+
+const Description = ({ children }: DescriptionProps) => {
+  return <div className={styles.Description}>{children}</div>;
+};
+
+const Image = ({ children }: ImageProps) => {
+  return <div>{children}</div>;
+};
+
+LendingFlexContent.Header = Header;
+LendingFlexContent.Description = Description;
+LendingFlexContent.Image = Image;
+
+export default LendingFlexContent;
