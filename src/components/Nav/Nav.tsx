02faa460eb38f658295166340a8c737670dc8b6e
gbwlxhd97 <gbwlxhd97@naver.com>
2024-05-11 14:17:16 +0900

    [feat]: Navbar

--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -1,7 +1,35 @@
-import React from 'react';
+import { Link } from 'gatsby';
+import { StaticImage } from 'gatsby-plugin-image';
+import React, { useState } from 'react';
+import * as styles from './Nav.module.scss';
 
 const Nav = () => {
-  return <nav>Nav</nav>;
+  const [isClickHamburger, setIsClickHamburger] = useState(false);
+  
+  return (
+    <nav className={styles.NavContainer}>
+      <div
+        className={`${styles.NavWrapper} ${isClickHamburger && styles.visible}`}
+      >
+        <div className={styles.LogoWrap}>
+          <Link to="/">
+            <StaticImage src="../../images/logo.svg" alt="logo" />
+          </Link>
+        </div>
+        <div className={styles.MenuWrap}>
+          <ul className={styles.Menu}>
+            <div className={`${styles.Item} ${true && styles.active}`}>
+              <li>서비스 소개</li>
+              <li>제휴 문의</li>
+            </div>
+            <li>
+              <button className={styles.Button}>혜택받고 등록하기</button>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
 };
 
 export default Nav;
