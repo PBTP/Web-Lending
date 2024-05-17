7290dc32aeeb6e91acdddbd8e9df94589846e9c9
gbwlxhd97 <gbwlxhd97@naver.com>
2024-05-18 00:44:34 +0900

    fix: Navbar responsive

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,16 +1,16 @@
 import React from "react";
 import Nav from "./Nav/Nav";
-
+import * as styles from './Layout.module.scss';
 type LayoutProps = {
     children: React.ReactNode;
 }
 
 const Layout = ({children}:LayoutProps) => {
   return (
-    <>
+    <div className={styles.LayoutContainer}>
         <Nav/>
         {children}
-    </>
+    </div>
   )
 }
 
