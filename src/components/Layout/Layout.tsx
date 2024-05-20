a76cdf65f7ed7e74f740c936782bfdddee74fc07
winju <winju@wemade.com>
2024-05-20 16:44:00 +0900

    [styles] AllianceMain mobile styles

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,17 +1,17 @@
 import React from "react";
 import Nav from "./Nav/Nav";
-import * as styles from './Layout.module.scss';
+import * as styles from "./Layout.module.scss";
 type LayoutProps = {
-    children: React.ReactNode;
-}
+  children: React.ReactNode;
+};
 
-const Layout = ({children}:LayoutProps) => {
+const Layout = ({ children }: LayoutProps) => {
   return (
     <div className={styles.LayoutContainer}>
-        <Nav/>
-        {children}
+      <Nav />
+      {children}
     </div>
-  )
-}
+  );
+};
 
 export default Layout;
