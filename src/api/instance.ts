056ffb49cb225a26b23e40e2d6e79920a53af162
winju <winju@wemade.com>
2024-05-29 21:14:54 +0900

    [chore]: gatsby root import delete, axios instance modify

--- src/api/instance.ts
+++ src/api/instance.ts
@@ -2,7 +2,6 @@ import axios from 'axios';
 
 export const requestApi = axios.create({
   baseURL: 'https://api.mgmg.life/',
-  timeout: 1000,
   headers: {
     'Content-Type': 'application/json',
   },
